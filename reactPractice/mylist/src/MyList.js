b6ee13264f29e57bb5fb3f0d5e17b56a6b4e6954
syun32 <leesyun983@gmail.com>
2023-06-24 02:13:02 +0900

    [React] Form 제출 및 알림 테스트

--- reactPractice/mylist/src/MyList.js
+++ reactPractice/mylist/src/MyList.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function MyList() {
-    const data = [1, 2, 3, 4, 5];
+    const [text, setText] = useState('');
+
+    const handleSubmit = (event) => {
+        alert(`You typed: ${text}`);
+        event.preventDefault();
+    }
 
     return (
-        <div>
-            <ul>
-                {
-                    data.map((number, index) =>
-                    <li key={index}>Listitem {number}</li>)
-                }
-            </ul>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={event => setText(event.target.value)} value={text}/>
+            <input type="submit" value="Press me"/>
+        </form>
     )
 }
 
